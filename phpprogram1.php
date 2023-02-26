ad6bfa49e6d75a0d54ca083ec57bc458a9b788a4
BheemSingh <bimlendukumarsingh.bks4@gmail.com>
2023-02-26 11:36:22 +0530

    updated

---
+++ phpprogram1.php
@@ -0,0 +1,12 @@
+//Creating php program
+
+
+<?php
+ 
+   echo "Let's start something exciting!";
+
+   $wealth = "destiny";
+
+   echo "Real wealth is $wealth";
+
+?>
---
+++ variable.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tech</title>
+</head>
+<body>
+<?php
+
+$google = "Study";
+
+
+echo "$google is the source of the knowledge.<br>" ; 
+echo "I love $google";
+
+
+$_comp = "computer";
+echo "<br>  Here, i am going to give knowledge of $_comp technology";
+
+$Maths4 = 90;
+$Maths45 = "90";
+$Maths46 = "90";
+
+
+echo "<br> I scored $Maths4 in Maths";
+echo "<br> In Maths i scored : "; echo $Maths4 ;
+
+echo "<br> I scored $Maths45+$Maths46 in Maths <br>";
+echo $Maths45+$Maths46;
+
+
+
+?>
+
+</body>
+</html>
+
